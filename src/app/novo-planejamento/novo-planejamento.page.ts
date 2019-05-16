82aea7cf1ae206bfaf598417adf27c73916feba6
luan verdelho <391758883@alunos.etec.itaqua>
2019-05-16 19:55:14 -0300

    Criação de tela de cadastro de planejamentos e navegações entre telas

---
+++ src/app/novo-planejamento/novo-planejamento.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-planejamento',
+  templateUrl: './novo-planejamento.page.html',
+  styleUrls: ['./novo-planejamento.page.scss'],
+})
+export class NovoPlanejamentoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
